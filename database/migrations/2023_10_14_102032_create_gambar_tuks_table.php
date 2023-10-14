6ade04a6e1d4d22b54029f7fb981bbf4a681125d
sepuhcode <sepuhcoding@gmail.com>
2023-10-14 17:39:18 +0700

    edit migrasi gambartuk

--- database/migrations/2023_10_14_102032_create_gambar_tuks_table.php
+++ database/migrations/2023_10_14_102032_create_gambar_tuks_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('gambar_tuks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('visibility');
             $table->timestamps();
         });
     }
