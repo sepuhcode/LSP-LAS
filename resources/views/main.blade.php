5247ff6f9282e94206025d2b27edcb681bbc0c14
sepuhcode <sepuhcoding@gmail.com>
2023-10-21 22:13:51 +0700

    Frontend Update

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,10 @@
     <!-- Tambahkan tautan ke file CSS Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href=" https://cdn.jsdelivr.net/npm/sweetalert2@11.7.31/dist/sweetalert2.min.css " rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
 </head>
 
 <nav class="navbar sticky-top navbar-expand-md navbar-light bg-white">
@@ -50,76 +53,147 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="{{ asset('images/Content-Dashboard.png') }}" class="d-block w-100 " alt="...">
+                <img src="{{ asset('images/Content-Dashboard1.png') }}" class="d-block w-100 " alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('images/Content-Dashboard.png') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('images/Content-Dashboard2.png') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('images/Content-Dashboard.png') }}" class="d-block w-100" alt="...">
+                <img src="{{ asset('images/Content-Dashboard1.png') }}" class="d-block w-100" alt="...">
             </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
     </div>
 
-    <div class="mx-2">
-        <div class="line-with-text">
-            <hr>
-            <div class="text">Daftar TUK Mandiri Aktif</div>
-            <hr>
+    <div class="mx-2 " style="background-color: #EFEFEF">
+       
+        <div class="TUK-left">
+            <h2>+18K</h2><br>
+            <p>Sertifikat Terbit</p>
+        </div>
+        <div class="TUK-mid">
+            <h2>18</h2><br>
+            <p>TUK Aktif</p>
+        </div>
+        <div class="TUK-right">
+            <h2>13</h2><br>
+            <p>Skema Sertifikasi</p>
         </div>
 
-        <div class="logos">
-            <div class="logos-slide">
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
+        <div class="row justify-content-center d-none d-lg-flex mb-4 mt-4">
+           <div class="col-lg-2 ">
+            <div>
+                <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
             </div>
-            <div class="logos-slide">
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
+           </div>
+           <div class="col-lg-2">
+            <div>
+                <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
             </div>
+           </div>
+           <div class="col-lg-2">
+            <div>
+                <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+            </div>
+           </div>
+           <div class="col-lg-2">
+            <div>
+                <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+            </div>
+           </div>
+           <div class="col-lg-2">
+            <div>
+                <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+            </div>
+           </div>
+           <div class="col-lg-2">
+            <div>
+                <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+            </div>
+           </div>
         </div>
+        <div class="row justify-content-center d-none d-lg-flex mb-4">
+            <div class="col-lg-2 ">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+         </div>
+         <div class="row justify-content-center d-none d-lg-flex">
+            <div class="col-lg-2 ">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+            <div class="col-lg-2">
+             <div>
+                 <img src="{{ asset('images/Patria.png') }}"  style="display: inline"/>
+             </div>
+            </div>
+         </div>
 
-        <div class="logos-2">
-            <div class="logos-slide-right">
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-            </div>
-            <div class="logos-slide-right">
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-                <img src="{{ asset('images/Patria.png') }}" />
-            </div>
-        </div>
     </div>
-
-    <div class="row mx-2 mt-2">
-        <div style="border-left: 3px solid red; border-top: 3px solid red; border-top-left-radius: 300px;">
+    <div class="carousel-inner-content2">
+        <img src="{{ asset('Images/Content-Dashboard.png') }}"/>
+    </div>
+    <div class="row mx-2 mt-2" style="background-color: #EFEFEF">
+        {{-- style="border-left: 3px solid red; border-top: 3px solid red; border-top-left-radius: 300px;"> --}}
+        <div> 
             <div class="col-12 text-center">
-                <h1 class="fw-semibold fs-4" style="color: red; position:relative; top: -15px;"><span style="background-color: white; position: relative; padding: 0 30px;">Visi Misi LSP LAS</span></h1>
-                <h2 class="fw-semibold mx-auto mt-3 mb-5 fs-5" style="color: #7B7878; width: 760px;">
+                <h1 class="fw-semibold fs-4" style="color: red; position:relative; top: 5px;"><span style= position: relative; padding: 0 30px;>Visi Misi LSP LAS</span></h1>
+                <h2 class="fw-semibold mx-auto mt-5 mb-5 fs-5" style="color: #959595; width: 760px;">
                     Menjadi satu – satunya Lembaga Sertifikasi Profesi Pengelasan di Indonesia yang memiliki Kompetensi Nasional dan Internasional untuk menghasilkan tenaga – tenaga pengelasan yang berkualifikasi Nasional dan Internasional.
                 </h2>
             </div>
@@ -216,15 +290,55 @@
             </div>
         </div>
     </div>
-
-    <div class="row mt-2">
-        <div class="alur-pendaftaran" style="padding: 3rem">
-            <div style="border-right: 3px solid white; border-top: 3px solid white; border-top-right-radius: 300px;">
-                <h1 class="fw-semibold fs-4 text-center" style="color: white; position:relative; top: -15px;"><span style="background-color: #FF2F2F; position: relative; padding: 0 30px;">Alur Pendaftaran</span></h1>
-                <div style="height: 500px"></div>
+<div class="footer">
+    <div class="container-footer">
+        <div class="row-footer" >
+            <div class="footer-col" style="margin-right: 20px">
+                <img src="{{ 'images/LogoLAS.png' }}" class="logo-footer"/>
+                <ul>
+                    <li><a href="#">Home </a></li>
+                    <li><a href="#">Sertifikasi</a></li>
+                    <li><a href="#">Pendaftaran</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+          
+            <div class="footer-col" style="margin-right: 20px">
+                <h4>Tentang Kami</h4>
+                <p>LSP LAS lembaga sertifikasi untuk
+                    profesi pengelasan yang didirikan
+                    oleh Asosiasi Pengelasan Indonesia
+                    dan Terlisensi oleh Badan Nasional
+                    Sertifikasi Profesi.</p>
+                
+                <h4>Contact Us</h4>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-regular fa-envelope"></i>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="footer-col" style="margin-right: 20px">
+                <h4>Alamat</h4>
+                <p style="text-align: justify">
+                    Graha Surveyor Indonesia Lt. 14
+                    Jl. Gatot Subroto Kav. 56 Jakarta Selatan
+                    Tlp : 0819 17100200
+                    Post code : 12950
+                </p>
+                
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.1850074093113!2d106.82989237581323!3d-6.2393303610991895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3c2f78e1417%3A0x6e69dc9146860164!2sPT%20Surveyor%20Indonesia%20(Persero)%20Tbk!5e0!3m2!1sid!2sid!4v1697885207960!5m2!1sid!2sid" 
+                width="500" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
             </div>
         </div>
     </div>
+</div>
+<footer>
+    <h5 style="text-align: center; background-color:#FEFEFE; margin:3">©2023 Copyright LSP LAS • All rights reserved • Made by nterco team</h5>
+</footer>
+    
 
     @if (request()->session()->has('failed'))
         <script>
@@ -250,7 +364,7 @@
 
         // show image after load dom
         $(document).ready(function(){
-            const imageUrl = "{{ asset('images/accordion-image-1.png') }}";
+            const imageUrl = "{{ asset('images/mampu-bersaing.png') }}";
             const imgElement = document.createElement("img");
             imgElement.src = imageUrl;
             imgElement.classList.add("img-fluid");
@@ -267,10 +381,10 @@
         accordionItems.forEach((item, index) => {
             const button = item.querySelector(".accordion-button");
             const images = [
-                "{{ asset('images/accordion-image-1.png') }}",
-                "{{ asset('images/accordion-image-2.png') }}",
-                "{{ asset('images/accordion-image-3.png') }}",
-                "{{ asset('images/accordion-image-4.png') }}",
+                "{{ asset('images/mampu-bersaing.png') }}",
+                "{{ asset('images/mengikuti-perkembangan.png') }}",
+                "{{ asset('images/membina-tempat.png') }}",
+                "{{ asset('images/menjalin hub.png') }}",
             ];
 
             button.addEventListener("click", () => {
