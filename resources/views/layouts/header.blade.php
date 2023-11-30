f2f9c9fc3418afb93df9baabcbecdecca476dbd8
AsadCus <icbpunvr2019@gmail.com>
2023-11-30 14:27:35 +0700

    :art: oke

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,9 @@
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse py-3 py-md-0" id="navbarNav">
             <a href="{{ url('/') }}" class="navbar-brand"><img src="{{ asset('images/LogoLAS.png') }}" width="120px"></a>
-            <ul class="navbar-nav ms-auto fw-bold">
+            <ul class="navbar-nav ms-auto me-5 fw-bold gap-4">
                 <li class="nav-item">
                     <a class="nav-link {{ Request::is('home') ? 'active' : '' }}" aria-current="page" href="{{ url('/') }}">Home</a>
                 </li>
@@ -19,15 +19,15 @@
                 <li class="nav-item">
                     <a class="nav-link {{ Request::is('about/*') ? 'active' : '' }}" href="#">About</a>
                 </li>
-                <li class="nav-item">
-                    @guest
-                        <a class="btn nav-link btn-primary" href="{{ url('/login') }}">Login</a>
-                    @endguest
-                    @auth
-                        <a class="btn nav-link btn-primary" href="{{ url('/logout') }}">Logout</a>
-                    @endauth
-                </li>
             </ul>
+        </div>
+        <div class="auth-section">
+            @guest
+                <a class="btn nav-link btn-primary" href="{{ url('/login') }}">Login</a>
+            @endguest
+            @auth
+                <a class="btn nav-link btn-primary" href="{{ url('/logout') }}">Logout</a>
+            @endauth
         </div>
     </div>
 </nav>
