2830c9c66dfa1830f859f6b6b3b5faba20a3c409
AsadCus <icbpunvr2019@gmail.com>
2024-12-02 04:11:43 +0700

    :art:

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -21,9 +21,9 @@
             <h4>Contact Us</h4>
             <div class="social-links">
                 <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-regular fa-envelope"></i>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#"><i class="fa-regular fa-envelope"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
             </div>
         </div>
         <div class="col-4 text-white">
