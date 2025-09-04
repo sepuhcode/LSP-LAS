6662afe49c06905c790e8dae84249ffcc775011a
enjoyyourcreaditcard <joywidiwibowo@gmail.com>
2025-09-04 18:18:39 +0700

    feat: Adds Export Heading

--- app/Exports/SurveillanceExport.php
+++ app/Exports/SurveillanceExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Surveillance;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class SurveillanceExport implements FromCollection
+class SurveillanceExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,29 @@ class SurveillanceExport implements FromCollection
     public function collection()
     {
         return Surveillance::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Nama Lengkap',
+            'Email',
+            'No Handphone',
+            'No Identitas',
+            'no Sertifikat',
+            'No Registrasi Sertifikat',
+            'Skema Kompetensi ID',
+            'Sumber Dana Sertifikasi ID',
+            'Nama Tempat Bekerja',
+            'Alamat Tempat Bekerja',
+            'Jabatan Ditempat Kerja',
+            'Proyek Sedang Dikerjakan',
+            'Jabatan Dalam Proyek',
+            'Pekerjaan Sesuai SKK',
+            'Pekerjaan Sesuai SKK Text',
+            'Dibuat Pertanggal',
+            'Diperbarui Pertanggal',
+        ];
     }
 }
